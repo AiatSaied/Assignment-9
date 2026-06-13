3727c99caf0ed8ef3b2d7797ef37ed16aaabaa32
AiatSaied <aiatsaied@gmail.com>
2026-06-13 03:44:46 +0300

    intial-commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// var addContactBtn = document.getElementById("addContactBtn");
+// var mainContent = document.getElementById("mainContent");
+
+// Variables
+const fullNameInput = document.getElementById("fullName");
+const phoneInput = document.getElementById("phoneNumber");
+const emailAddressInput = document.getElementById("emailAddress");
+const addressInput = document.getElementById("address");
+const groupInput = document.getElementById("group");
+const noteInput = document.getElementById("notes");
+const isFav = document.getElementById("isFavorite");
+const isEmergency = document.getElementById("isEmergency");
+const saveContactBtn = document.getElementById("saveContactBtn");
+
+const updateContactBtn = document.getElementById("updateContactBtn");
